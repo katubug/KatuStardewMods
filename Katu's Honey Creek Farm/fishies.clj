734b660fa7c3f5d026506426c2f74f2a72873866
Katu <captain.katu@gmail.com>
2024-07-10 15:41:24 -0500

    all files sorted and symlinked and stuff!

---
+++ Katu's Honey Creek Farm/fishies.clj
@@ -0,0 +1,67 @@
+waterfall fish: DONE
+1-7
+[all]
+- goby "Goby"
+- woodskip "734"
+[spring]
+[summer]
+[fall]
+- salmon "139"
+[winter]
+
+lake fish: DONE
+1-3
+[all]
+- largemouth bass "136"
+- carp "142"
+- bullhead "700"
+[spring]
+[summer]
+- rainbow trout "138"
+- dorado "704"
+- sturgeon "698"
+[fall]
+[winter]
+- lingcod "707"
+
+river fish: DONE
+1-4
+[all]
+- bream "132"
+- chub "702"
+[spring]
+- shad "706"
+- sunfish "145"
+[summer]
+- sunfish "145"
+- shad "706"
+- rainbow trout "138"
+- pike "144"
+[fall]
+- salmon "139"
+- tiger trout "699"
+- walleye "140"
+- shad "706"
+[winter]
+- walleye "140"
+- perch "141"
+
+pond fish:  DONE
+1-3
+[all]
+- carp "142"
+- woodskip "734"
+[spring]
+- smallmouth bass "137"
+- catfish "143"
+[summer]
+- catfish "143"
+- pike "144"
+[fall]
+- walleye "140"
+- midnight carp "269"
+- smallmouth bass "137"
+- catfish "143"
+[winter]
+- walleye "140"
+- perch "141"
